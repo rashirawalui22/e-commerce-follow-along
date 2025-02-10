b1698c4cf3dc8738cc6471e5f22301cdb5987eb6
rashirawal22 <rashi.rawal@kalvium.community>
2025-02-10 15:36:40 +0530

    Assignment till 13

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,6 @@
+import LoginPage from "./pages/Login.jsx";
+import SignupPage from "./pages/Signup.jsx";
+import Home from "./pages/Home.jsx";
+import CreateProduct from './pages/createProduct.jsx';
+import MyProducts from "./pages/myProducts.jsx";
+export {LoginPage,SignupPage,Home,CreateProduct,MyProducts};
